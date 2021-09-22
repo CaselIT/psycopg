mattip/psycopg3

Make encodings module private

tests/test_connection.py
@@ -8,7 +8,7 @@ import weakref
 from threading import Thread
 
 import psycopg3
-from psycopg3 import encodings
+from psycopg3._encodings import pg2pyenc
 from psycopg3 import Connection, Notify
 from psycopg3.rows import tuple_row
 from psycopg3.errors import UndefinedTable
@@ -294,7 +294,7 @@ def test_autocommit_unknown(conn):
 
 def test_get_encoding(conn):
     (enc,) = conn.cursor().execute("show client_encoding").fetchone()
-    assert conn.client_encoding == encodings.pg2py(enc)
+    assert conn.client_encoding == pg2pyenc(enc)
 
 
 @pytest.mark.parametrize(

tests/test_connection_async.py
@@ -7,11 +7,11 @@ import logging
 import weakref
 
 import psycopg3
-from psycopg3 import encodings
 from psycopg3 import AsyncConnection, Notify
 from psycopg3.rows import tuple_row
 from psycopg3.errors import UndefinedTable
 from psycopg3.conninfo import conninfo_to_dict
+from psycopg3._encodings import pg2pyenc
 from .test_cursor import my_row_factory
 
 pytestmark = pytest.mark.asyncio
@@ -306,7 +306,7 @@ async def test_get_encoding(aconn):
     cur = aconn.cursor()
     await cur.execute("show client_encoding")
     (enc,) = await cur.fetchone()
-    assert aconn.client_encoding == encodings.pg2py(enc)
+    assert aconn.client_encoding == pg2pyenc(enc)
 
 
 @pytest.mark.parametrize(

CaselIT/psycopg

tests/test_connection.py
@@ -7,7 +7,7 @@ import weakref
 from threading import Thread
 
 import psycopg
-from psycopg import encodings
+from psycopg._encodings import pg2pyenc
 from psycopg import Connection, Notify
 from psycopg.rows import tuple_row
 from psycopg.errors import UndefinedTable
@@ -300,7 +300,7 @@ def test_autocommit_unknown(conn):
 
 def test_get_encoding(conn):
     (enc,) = conn.cursor().execute("show client_encoding").fetchone()
-    assert conn.client_encoding == encodings.pg2py(enc)
+    assert conn.client_encoding == pg2pyenc(enc)
 
 
 @pytest.mark.parametrize(

tests/test_connection_async.py
@@ -6,11 +6,11 @@ import logging
 import weakref
 
 import psycopg
-from psycopg import encodings
 from psycopg import AsyncConnection, Notify
 from psycopg.rows import tuple_row
 from psycopg.errors import UndefinedTable
 from psycopg.conninfo import conninfo_to_dict, make_conninfo
+from psycopg._encodings import pg2pyenc
 
 from .utils import gc_collect
 from .test_cursor import my_row_factory
@@ -313,7 +313,7 @@ async def test_get_encoding(aconn):
     cur = aconn.cursor()
     await cur.execute("show client_encoding")
     (enc,) = await cur.fetchone()
-    assert aconn.client_encoding == encodings.pg2py(enc)
+    assert aconn.client_encoding == pg2pyenc(enc)
 
 
 @pytest.mark.parametrize(